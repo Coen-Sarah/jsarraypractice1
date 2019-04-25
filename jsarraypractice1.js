53c0961e873049c26a3ac869b611878f391481a7
Coen_Sarah <sarah.e.coen.07@gmail.com>
2019-04-25 13:20:14 -0500

    .range function project upload

---
+++ jsarraypractice1.js
@@ -0,0 +1,9 @@
+const range = function (start,end){
+    let total = start;
+    while (start < end){
+        total = total + (start + 1);
+        start++ ;
+    }
+    return total;
+}
+console.log(range(1,5))
